c4710eff3587e3473214b458e1438a1e6c4e9868
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-03-10 14:30:40 +0000

    Update 5 files

--- controllers/auth/signupUserController.js
+++ controllers/auth/signupUserController.js
@@ -1,6 +1,8 @@
 const { Conflict } = require("http-errors");
 const { User } = require("../../models");
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
+const { sendEmail } = require("../../helpers");
 
 const signupUserController = async (req, res) => {
   const { email, password } = req.body;
@@ -8,18 +10,22 @@ const signupUserController = async (req, res) => {
 
   if (user) throw new Conflict("Email in use");
 
+  const verificationToken = uuidv4();
+
   const avatarURL = gravatar.url(email);
-  const newUser = new User({ email, avatarURL });
+
+  const newUser = new User({ email, avatarURL, verificationToken });
 
   newUser.setPassword(password);
 
-  newUser.save();
+  await newUser.save();
 
   res.status(201).json({
     user: {
       email,
       subscription: "starter",
       avatarURL,
+      verificationToken,
     },
   });
 };
