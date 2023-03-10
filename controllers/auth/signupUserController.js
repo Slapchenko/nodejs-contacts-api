3ec06c6ebd54ff025555bd3fa9663413fb33c78d
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-03-10 15:06:43 +0000

    Update signupUserController.js and verifyEmailController.js

--- controllers/auth/signupUserController.js
+++ controllers/auth/signupUserController.js
@@ -6,44 +6,40 @@ const { sendEmail } = require("../../helpers");
 const { PORT } = process.env;
 
 const signupUserController = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (user) {
-      throw new Conflict("Email is already in use");
-    }
-
-    const verificationToken = uuidv4();
-
-    const avatarURL = gravatar.url(email);
-
-    const newUser = new User({ email, avatarURL, verificationToken });
-
-    newUser.setPassword(password);
-
-    await newUser.save();
-
-    const mail = {
-      to: email,
-      subject: "Email Confirmation",
-      html: `<a target="_blank" href="http://localhost:${PORT}/api/users/verify/${verificationToken}">Confirm Email</a>`,
-    };
-
-    await sendEmail(mail);
-
-    res.status(201).json({
-      user: {
-        email,
-        subscription: "starter",
-        avatarURL,
-        verificationToken,
-      },
-    });
-  } catch (error) {
-    next(error);
+  if (user) {
+    throw new Conflict("Email is already in use");
   }
+
+  const verificationToken = uuidv4();
+
+  const avatarURL = gravatar.url(email);
+
+  const newUser = new User({ email, avatarURL, verificationToken });
+
+  newUser.setPassword(password);
+
+  await newUser.save();
+
+  const mail = {
+    to: email,
+    subject: "Email Confirmation",
+    html: `<a target="_blank" href="http://localhost:${PORT}/api/users/verify/${verificationToken}">Confirm Email</a>`,
+  };
+
+  await sendEmail(mail);
+
+  res.status(201).json({
+    user: {
+      email,
+      subscription: "starter",
+      avatarURL,
+      verificationToken,
+    },
+  });
 };
 
 module.exports = signupUserController;
