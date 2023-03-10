bc69078319bbc2cce9cb768bdb3051b2a4e3af6b
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-03-10 15:25:39 +0000

    Update loginUserController.js

--- controllers/auth/loginUserController.js
+++ controllers/auth/loginUserController.js
@@ -8,13 +8,21 @@ const loginUserController = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  const validPassword = user.comparePassword(password);
 
-  if (!user || !validPassword)
+  if (!user) {
+    throw new Unauthorized("User not found");
+  }
+
+  const validPassword = user.comparePassword(password);
+  if (!validPassword) {
     throw new Unauthorized("Email or password is wrong");
+  }
+
+  if (!user.verify) {
+    throw new Unauthorized("User is not verified");
+  }
 
   const payload = { id: user._id };
-
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "10h" });
 
   await User.findByIdAndUpdate(user._id, { token });
